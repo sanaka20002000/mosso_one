1e7b3c5e87b86ac00d7bdd597f4f5ab4086e02f2
sanaka20002000 <sanaka20002000@gmail.com>
2016-04-21 13:27:43 -0500

    first commit

---
+++ dogface/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from dogface.models import TEST
+# Register your models here.
+
+admin.site.register(TEST)
---
+++ dogface/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DogfaceConfig(AppConfig):
+    name = 'dogface'
